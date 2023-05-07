1fa7f48b9343acf220578e8a2946fcb2d4cde07a
MickKav <>
2023-05-07 16:41:32 +0000

    Add favicon, and final tidy ups for the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    }) 
+
     runGame(addition);
 })
 /**
@@ -19,6 +26,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
